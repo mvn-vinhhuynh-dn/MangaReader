58a6a7abc45b958b4d412f9c92c081ad22f26052
at-vinhhlb <vinhhlb@asiantech.vn>
2016-09-23 15:09:37 +0700

    Initial commit

---
+++ app/src/main/java/coder/victorydst3/mangareader/MainActivity.java
@@ -0,0 +1,14 @@
+package coder.victorydst3.mangareader;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+    @AfterViews
+    void afterViews(){
+    }
+}
