5ba7b97acd3d208df63a81bff78287a1f64068e6
at-vinhhlb <vinhhlb@asiantech.vn>
2016-09-26 11:10:18 +0700

    update prj

--- app/src/main/java/coder/victorydst3/mangareader/MainActivity.java
+++ app/src/main/java/coder/victorydst3/mangareader/MainActivity.java
@@ -8,6 +8,7 @@ import org.androidannotations.annotations.EActivity;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
     public int A;
+    public int B;
     @AfterViews
     void afterViews(){
     }
