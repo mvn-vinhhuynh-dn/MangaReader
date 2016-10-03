ae8caa450839c9c02b34a48a7b56b246479ba6e7
at-vinhhlb <vinhhlb@asiantech.vn>
2016-10-03 15:16:04 +0700

    fix double divider

--- app/src/main/java/coder/victorydst3/mangareader/widget/SpacesItemDecoration.java
+++ app/src/main/java/coder/victorydst3/mangareader/widget/SpacesItemDecoration.java
@@ -20,12 +20,14 @@ public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
     public void getItemOffsets(Rect outRect, View view,
                                RecyclerView parent, RecyclerView.State state) {
         outRect.left = space;
-        outRect.right = space;
         outRect.bottom = space;
 
         // Add top margin only for the first item to avoid double space between items
         if (parent.getChildLayoutPosition(view) == 0) {
             outRect.top = space;
+            outRect.right = space;
+        } else if (parent.getChildAdapterPosition(view) % 3 == 0) {
+            outRect.right = space;
         } else {
             outRect.top = 0;
         }
